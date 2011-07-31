3b0329a305ba0769b6838093b8f230579f980b09
Jason McCandless <jasonmccandless@gmail.com>
2011-07-31 15:38:06 +0100

    Clean up code slightly.

--- meteorsms.py
+++ meteorsms.py
@@ -2,101 +2,100 @@ import mechanize
 from BeautifulSoup import BeautifulSoup
 import re
 import urllib
-from urlparse import urljoin
 
 
-import sys, logging
-logger = logging.getLogger("mechanize")
-logger.addHandler(logging.StreamHandler(sys.stdout))
-logger.setLevel(logging.INFO)
+# import sys, logging
+# logger = logging.getLogger("mechanize")
+# logger.addHandler(logging.StreamHandler(sys.stdout))
+# logger.setLevel(logging.INFO)
 #logger.setLevel(logging.DEBUG)
 
 
-
-br = mechanize.Browser()
-
-br.addheaders = [("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2.7) Gecko/20100713 Firefox/3.6.7")]
+USERNAME = "REDACTED"
+PASSWORD = "REDACTED"
 
 
-br.open("http://meteor.ie")
-
-
-for f in br.forms():
-    if f.attrs['id'] == "MyMeteorLogin":
-        loginForm = f
-
-assert(loginForm)
-
-br.form = loginForm
-
-br.set_handle_robots(False)
-
-
-br["msisdn"] = "REDACTED"
-br["pin"] = "REDACTED"
-
-r2 = br.submit()
-
-
-l = br.find_link(url_regex="freeweb")
-
-br.follow_link(l)
+def getphonebook():
+    """Parse the meteorsms phoenbook"""
+    pass
 
 
 
+def send_text(number,text):
 
-d = br.response().get_data()
-soup = BeautifulSoup(d)
-print "Texts remaining:", soup.find(id = "numfreesmstext")['value']
+    assert(number.isdigit())
 
+    br = mechanize.Browser()
 
-regex = re.compile("var CFID = (\d*);")
-r = regex.search(d)
-cfid = r.groups()[0]
-regex = re.compile("var CFTOKEN = (\d*);")
-r = regex.search(d)
-cftoken = r.groups()[0]
+    #TODO: save and loading of cookies!
+
+    br.addheaders = [("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2.7) Gecko/20100713 Firefox/3.6.7")]
+    br.set_handle_robots(False) # sorry, but otherwise it looks for a non-existent robots.txt and seems to block
 
 
-
-#we can't click anything anymore, let's do the ajax requests ourselves
-
+    br.open("http://meteor.ie")
 
 
-number = "0|REDACTED" #TODO: might need to prepend "0|"
-
-number = urllib.quote(number.encode("utf-8"))
-
-
-#geturl "/mymeteorapi/index.cfm?event=smsAjax&CFID=99999&CFTOKEN=99999&func=addEnteredMsisdns"
-#urlencoded form query:  "ajaxRequest=addEnteredMSISDNs&remove=-&add="+encodeURI(number)
-
-url = "/mymeteorapi/index.cfm?event=smsAjax&CFID=%s&CFTOKEN=%s&func=addEnteredMsisdns" % (cfid,cftoken)
-url = urljoin(br.geturl(),url)
-data = "ajaxRequest=addEnteredMSISDNs&remove=-&add="+number
-
-r1 = br.open(url,data)
+    for f in br.forms():
+        if f.attrs['id'] == "MyMeteorLogin":
+            loginForm = f
+    assert(loginForm)
+    br.form = loginForm
 
 
+    br["msisdn"] = USERNAME
+    br["pin"] = PASSWORD
+
+    login_response = br.submit()
+
+
+    br.follow_link(br.find_link(url_regex="freeweb"))
+
+
+    data = br.response().get_data()
+    soup = BeautifulSoup(data)
+    print "Texts remaining:", soup.find(id = "numfreesmstext")['value']
+
+
+    regex = re.compile("var CFID = (\d*);")
+    try:
+        r = regex.search(data)
+        cfid = r.groups()[0]
+    except:
+        raise Exception("Could not get CFID!")
+    regex = re.compile("var CFTOKEN = (\d*);")
+    try:
+        r = regex.search(data)
+        cftoken = r.groups()[0]
+    except:
+        raise Exception("Could not get CFTOKEN!")
 
 
 
+    #we can't click anything anymore, let's do the ajax requests ourselves
+
+    url = "/mymeteorapi/index.cfm?event=smsAjax&CFID=%s&CFTOKEN=%s&func=addEnteredMsisdns" % (cfid,cftoken)
+    data = "ajaxRequest=addEnteredMSISDNs&remove=-&add=" + urllib.quote(("0|" + number).encode("utf-8"))
+    r1 = br.open(url,data)
 
 
-text = "can't believe it works!!"
+    url = "/mymeteorapi/index.cfm?event=smsAjax&func=sendSMS&CFID=%s&CFTOKEN=%s" %(cfid,cftoken)
+    data = "ajaxRequest=sendSMS&messageText=" + urllib.quote(text.encode("utf-8"))
+    r2 = br.open(url,data)
 
-text = urllib.quote(text.encode("utf-8"))
+    #TODO: ensure these responses are 200 OK and not 301 moved temporarily
 
-#geturl "/mymeteorapi/index.cfm?event=smsAjax&func=sendSMS&CFID=REDACTED&CFTOKEN=REDACTED"
-#urlencoded form query: "ajaxRequest=sendSMS&messageText=" + encodeURI(text)
-
-url = "/mymeteorapi/index.cfm?event=smsAjax&func=sendSMS&CFID=%s&CFTOKEN=%s" %(cfid,cftoken)
-url = urljoin(br.geturl(),url)
-data = "ajaxRequest=sendSMS&messageText=" + text
-
-r2 = br.open(url,data)
+    #TODO: make these neater by using encodeuri to build the data part
 
 
+
+def main():
+    
+    send_text("REDACTED","cool")
+
+
+if __name__ == "__main__":
+    main()
 
 
 
